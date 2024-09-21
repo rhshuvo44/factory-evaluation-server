9f78107fe88cd6aec6f3fda4774f4d450447ce76
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-21 17:36:21 +0600

    Miscellaneous validation Zod schema

---
+++ src/app/module/miscellaneous/miscellaneous.validation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const travelValidation = z.object({
+    body: z.object({
+      date: z.string(),
+      particulars: z.string(),
+      description: z.string(),
+      remark: z.string().optional(),
+      buyerId: z.string().optional(),
+      orderNo: z.string(),
+      payTo: z.string().optional(),
+      paymentType: z.enum(['Monthly', 'Daily', 'Once']),
+      unit: z.string(),
+      unitPrice: z.number().positive(),
+      totalPrice: z.number().positive(),
+    }),
+  })
