91cddaf992a33b675287a767d6867de4f8b0414c
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-21 17:33:00 +0600

    Miscellaneous interface and model schema

---
+++ src/app/module/miscellaneous/miscellaneous.interface.ts
@@ -0,0 +1,14 @@
+export type TMiscellaneous = {
+    slNo: number
+    date: Date
+    particulars: string
+    description: string
+    remark?: string
+    buyerId?: string
+    orderNo?: string
+    payTo: string
+    paymentType: 'Monthly' | 'Daily' | 'Once'
+    unit?: string
+    unitPrice: number
+    totalPrice: number
+}
